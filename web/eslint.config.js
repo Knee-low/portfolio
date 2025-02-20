8a859397840ca474beccad9ebb049382a6f6cda9
NCJ <168267086+Knee-low@users.noreply.github.com>
2025-02-21 01:14:23 +0800

    Create workflow (#2)
    * sample
    
    * Transfer
    
    * with console log
    
    * Added github actions
    
    * Trigger workflow
    
    * Configure YML
    
    * Trigger workflow
    
    * Trigger workflow
    
    * added console
    
    * Test linter
    
    * Removed console logs
    
    * test linters
    
    * removed console logs

---
+++ web/eslint.config.js
@@ -0,0 +1,23 @@
+const eslintPluginTypeScript = await import("@typescript-eslint/eslint-plugin");
+const eslintParserTypeScript = await import("@typescript-eslint/parser");
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+  {
+    ignores: ["**/*.js", ".next/**", "node_modules/**"],
+  },
+  {
+    files: ["**/*.{ts,tsx,jsx}"],
+    languageOptions: {
+      parser: eslintParserTypeScript.default,
+      sourceType: "module",
+    },
+    plugins: {
+      "@typescript-eslint": eslintPluginTypeScript.default,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "error",
+      "no-console": "error",
+    },
+  },
+];
