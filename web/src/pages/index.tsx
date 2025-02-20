d615970d3c00009253f286ce640a7209d77cd127
knee-low <castillanonilojr@gmail.com>
2025-02-21 01:33:43 +0800

    Rebased

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -12,6 +12,7 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+<<<<<<< Updated upstream
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
@@ -111,4 +112,7 @@ export default function Home() {
       </footer>
     </div>
   );
+=======
+  return <NiloJrPage />;
+>>>>>>> Stashed changes
 }
